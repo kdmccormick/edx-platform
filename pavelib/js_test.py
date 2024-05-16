ed86c7f8d75f79d54c4815fe8a6c151d4c4dcd13
Kyle D. McCormick <kyle@axim.org>
2024-05-16 19:20:26 -0400

    feat!: remove paver assets, prereqs (needs compile-reqs)

--- pavelib/js_test.py
+++ pavelib/js_test.py
@@ -7,8 +7,6 @@ import os
 import re
 import sys
 
-from paver.easy import cmdopts, needs, task
-
 from pavelib.utils.envs import Env
 from pavelib.utils.test.suites import JestSnapshotTestSuite, JsTestSuite
 from pavelib.utils.timer import timed
